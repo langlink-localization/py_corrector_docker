5b6232e9c2b5a8646173b2027b42a8654042f698
warecast <tk4900@gmail.com>
2024-05-21 11:33:10 +0800

    改为简单的flask启动

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,14 @@ WORKDIR /app
 COPY . /app
 
 # 安装依赖
+RUN pip3 install torch --index-url https://download.pytorch.org/whl/cpu --no-cache-dir
 RUN pip install --no-cache-dir -r requirements.txt
 
+# 设置 Flask 应用的环境变量
+ENV FLASK_APP=app.py
+
+# 设置适当的文件权限以确保日志可以写入
+RUN touch /app/app.log && chmod 666 /app/app.log
+
 # 启动命令
-CMD ["uwsgi", "--ini", "uwsgi.ini"]
+CMD ["flask", "run", "--host=0.0.0.0", "--port=5001"]
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask, request, jsonify
 from pycorrector import MacBertCorrector
-# from pycorrector import ErnieCscCorrector
 import json
 
 app = Flask(__name__)
@@ -8,13 +9,24 @@ app.config['JSON_AS_ASCII'] = False
 
 # 加载预训练模型
 m = MacBertCorrector('shibing624/macbert4csc-base-chinese')
-# m = ErnieCscCorrector()
 
+# 配置日志
+if not app.debug:
+    # 在生产环境下配置日志
+    file_handler = RotatingFileHandler('app.log', maxBytes=10240, backupCount=10)
+    file_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    file_handler.setFormatter(formatter)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Flask startup')
 
 @app.route('/correct', methods=['POST'])
 def correct_text():
     data = request.get_json()
     if not data or 'text' not in data:
+        app.logger.error('No text provided')
         return jsonify({'error': 'No text provided'}), 400
 
     text = data['text']
@@ -22,35 +34,8 @@ def correct_text():
     response = json.dumps({
         'corrected_result': corrected_result
     }, ensure_ascii=False)
+    app.logger.info(f'Corrected text: {corrected_result}')
     return app.response_class(response, content_type='application/json')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
-
-# from flask import Flask, request, jsonify
-# from paddlenlp import Taskflow  # 引入 Taskflow
-# import json
-
-# app = Flask(__name__)
-# app.config['JSON_AS_ASCII'] = False
-
-# # 加载预训练模型，使用 text_correction
-# corrector = Taskflow("text_correction")
-
-# @app.route('/correct', methods=['POST'])
-# def correct_text():
-#     data = request.get_json()
-#     if not data or 'text' not in data:
-#         return jsonify({'error': 'No text provided'}), 400
-
-#     text = data['text']
-#     # 使用 Taskflow 进行纠错
-#     corrected_result = corrector(text)
-#     response = json.dumps({
-#         'corrected_result': corrected_result
-#     }, ensure_ascii=False)
-#     return app.response_class(response, content_type='application/json')
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5001)
